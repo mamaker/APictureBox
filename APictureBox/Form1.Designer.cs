7496e613989a7f962930c8836556b8d1b4db724b
mamaker <madhuvasudevan@hotmail.com>
2015-04-21 23:05:33 -0500

    Finally!

--- APictureBox/Form1.Designer.cs
+++ APictureBox/Form1.Designer.cs
@@ -28,12 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.GoodButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(28, 261);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(496, 195);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // GoodButton
+            // 
+            this.GoodButton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.GoodButton.Location = new System.Drawing.Point(28, 105);
+            this.GoodButton.Name = "GoodButton";
+            this.GoodButton.Size = new System.Drawing.Size(146, 40);
+            this.GoodButton.TabIndex = 1;
+            this.GoodButton.Text = "Good Evening!";
+            this.GoodButton.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 481);
+            this.Controls.Add(this.GoodButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button GoodButton;
     }
 }
 
